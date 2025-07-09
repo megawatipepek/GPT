4c0b4ed1d601e13792ed202674062dd31f8973f7
megawatipepek <144662266+megawatipepek@users.noreply.github.com>
2025-07-09 14:03:27 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('surpriseBtn').addEventListener('click', function() {
-  const message = document.getElementById('hiddenMessage');
-  message.classList.remove('hidden');
-  
-  // Animasi tambahan
-  document.body.style.backgroundColor = "#ffe0f0";
-  this.textContent = "I LOVE YOU! 💘";
-});
